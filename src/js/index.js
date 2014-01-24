7402df40508a16c903ef6c351bedb3de8bb159dd
Danny Brian <dbrian@conceptuary.com>
2014-01-23 23:30:04 -0700

    Added more interaction between the scrollers to demonstrate custom events. Also added a loader example when the green button is tapped.

--- src/js/index.js
+++ src/js/index.js
@@ -18,9 +18,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	// Add scrolling to elements with iScroll
 	var optionsMenu = new IScroll( document.querySelector('[data-tulito-id="back-pane-left"]'), { eventPassthrough: false, scrollX: false, scrollY: true, snap: false } );
-	app.scroller1 = new IScroll('#hscroller1', { eventPassthrough: true, scrollX: true, scrollY: false, snap: false, probeType: 3 });
-	app.scroller2 = new IScroll('#hscroller2', { eventPassthrough: true, scrollX: true, scrollY: false, snap: false, probeType: 3 });
-	app.scroller3 = new IScroll('#hscroller3', { eventPassthrough: true, scrollX: true, scrollY: false, snap: true, snapStepX: window.innerWidth - 31 });
+	app.scroller1 = new IScroll('#hscroller1', { eventPassthrough: true, scrollX: true, scrollY: false, probeType: 3, deceleration: 0.001 });
+	app.scroller2 = new IScroll('#hscroller2', { eventPassthrough: true, scrollX: true, scrollY: false, probeType: 3, deceleration: 0.001 });
+	app.scroller3 = new IScroll('#hscroller3', { eventPassthrough: true, scrollX: true, scrollY: false, snap: true, snapStepX: window.innerWidth - 31, deceleration: 0.008 });
 	
 	// Make the two small carousels highlight the middle button
 	var liNum1 = 3;
@@ -54,10 +54,11 @@ document.addEventListener('DOMContentLoaded', function() {
 	// The buttons in the scroller do not have data-tulito-open attributes because we 
 	// want to handle this ourselves, opening the bottom pane to the matching content.
 	// This is a good illustration of where tulito stops being helpful in order to avoid 
-	// too much abstraction in the library.
-	var buttons = document.querySelectorAll('#hscroller1 li');
-	for (var i = 0; i < buttons.length; ++i) {
-		Hammer(buttons[i]).on("tap", function(e) {
+	// too much abstraction in the library. For the first scroller, we'll make this open
+	// the equivalent card in the bottom pane.
+	var buttons1 = document.querySelectorAll('#hscroller1 li');
+	for (var i = 0; i < buttons1.length; ++i) {
+		Hammer(buttons1[i]).on("tap", function(e) {
 			var scrollid = e.srcElement.getAttribute('data-scrollid');
 			var targetel = document.querySelector('#hscroller3 * [data-scrollid="' + scrollid + '"]');
 			tulito.toggleOpen(document.querySelector('[data-tulito-id="bottom-pane"]'), e);
@@ -68,6 +69,39 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 	}
 	
+	// For the second scroller, we'll have the buttons remove the corresponding button in the
+	// upper scroller.
+	var buttons2 = document.querySelectorAll('#hscroller2 li');
+	for (var i = 0; i < buttons2.length; ++i) {
+		Hammer(buttons2[i]).on("tap", function(e) {
+			var scrollid = e.srcElement.getAttribute('data-scrollid');
+			var targetel = document.querySelector('#hscroller1 * [data-scrollid="' + scrollid + '"]');
+			// Let the transition happen, then remove the element.
+			//targetel.addEventListener('transitionend', function(e) {
+			//	if (e.propertyName === 'width') {
+					targetel.parentNode.removeChild(targetel);
+					document.querySelector('#hscroller1').style.width = (64 * document.querySelectorAll('#hscroller1 li').length) + "px";
+					setTimeout(function() {
+						app.scroller1.refresh();
+					}, 100);
+			//	}
+			//}, false);
+			//tulito._addClass(targetel, 'removing');
+		});
+	}
+	
+	Hammer(document.querySelector('#thanks-button')).on("tap", function(e) {
+		var loader = document.querySelector('.loader');
+		tulito._addClass(loader, 'shown');
+		setTimeout(function() {
+			tulito._addClass(loader, 'opened');
+		}, 1);
+		setTimeout(function() {
+			tulito._removeClass(loader, 'opened');
+			setTimeout(function() { tulito._removeClass(loader, 'shown') }, 500);
+			tulito.toggleOpen(document.querySelector('[data-tulito-id="thanks-pane"]'));
+		}, 2000);
+	});
 	// Initialize tulito
 	tulito.init(
 		{
